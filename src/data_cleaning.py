87a542f132be46271bd28c273bb37e17ebd71015
__EnderShade__ <decicconunzio@gmail.com>
2025-04-25 20:53:34 +0200

    Update and rename df.py to src/data_cleaning.py

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -2,6 +2,5 @@ import pandas as pd
 
 def dataframe():
     df = pd.read_csv('https://raw.githubusercontent.com/MaSTERmIKK/data_-marvel/refs/heads/main/data/spotify-charts-daily-2020-10-07.csv')
-    
     return df
 
